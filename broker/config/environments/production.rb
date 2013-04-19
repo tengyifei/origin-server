fb08794d2707110d799c6301a4ca90dddf0f2fe0
Diego Castro <spinolacastro@gmail.com>
2013-04-19 12:42:13 -0300

    blacklist implementation

--- broker/config/environments/production.rb
+++ broker/config/environments/production.rb
@@ -72,6 +72,7 @@ Broker::Application.configure do
     :scopes => ['Scope::Session', 'Scope::Read', 'Scope::Application', 'Scope::Userinfo'],
     :default_scope => 'userinfo',
     :scope_expirations => OpenShift::Controller::Configuration.parse_expiration(conf.get('AUTH_SCOPE_TIMEOUTS'), 1.day),
+    :blacklist => conf.get("BLACKLISTED_WORDS", "").split(","),
   }
 
   config.auth = {
