c2be59664f5524f75637a9f0f1c81a6ad381293e
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2014-04-01 16:07:13 -0300

    missing include

--- getup-console/app/helpers/console/html5_boilerplate_helper.rb
+++ getup-console/app/helpers/console/html5_boilerplate_helper.rb
@@ -1,4 +1,6 @@
 module Console::Html5BoilerplateHelper
+  include Console::UserManagerHelper
+
   # Create a named haml tag to wrap IE conditional around a block
   # http://paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither
   def ie_tag(name=:body, attrs={}, &block)
